13276f325a6fc195d292fa6ff07b56aa698896b3
Samakshg11 <samakshgarg2005@gmail.com>
2026-03-26 20:53:39 +0530

    Fix formatting in About component description for improved readability

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-        Full Stack Developer specializing in React.js, Node.js, Express.js, and modern JavaScript frameworks. Experienced in building scalable web applications, RESTful APIs, and interactive user interfaces. Passionate about developing high-performance applications and turning ideas into real-world products through clean code and efficient architecture.
+        Full Stack Developer specializing in React.js, Node.js, Express.js, and modern JavaScript frameworks Experienced in building scalable web applications, RESTful APIs, and interactive user interfaces. Passionate about developing high-performance applications and turning ideas into real-world products through clean code and efficient architecture.
         </p>
       </div>
     </div>
